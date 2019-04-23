31638118c4d47516311978d75a1b922c4191fba7
Maryniuk-SV <mefissto.w@gmail.com>
2019-04-23 17:34:13 +0300

    added tests for login and registrationcomponents

--- client/src/app/public/components/login/login.component.spec.ts
+++ client/src/app/public/components/login/login.component.spec.ts
@@ -1,14 +1,25 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormsModule,
+  ReactiveFormsModule,
+  AbstractControl,
+} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
+import { DebugElement } from '@angular/core';
+import { of } from 'rxjs';
 
 import { LoginComponent } from './login.component';
 import { HeaderComponent } from '../header/header.component';
+import { LoginService } from '../../../core/services/auth/login.service';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let de: DebugElement;
+  let mockToken;
+  let service: LoginService;
+  let spy: jasmine.Spy;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -19,16 +30,56 @@ describe('LoginComponent', () => {
         HttpClientModule,
       ],
       declarations: [LoginComponent, HeaderComponent],
+      providers: [LoginService],
     }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement;
+    mockToken = { token: 'some token' };
+    service = de.injector.get(LoginService);
+    spy = spyOn(service, 'login').and.returnValue(of(mockToken));
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('form should be valid', () => {
+    component.loginForm.controls['email'].patchValue('some@gmail.com');
+    component.loginForm.controls['password'].patchValue('password');
+    expect(component.loginForm.valid).toBeTruthy();
+  });
+
+  it('form should be invalid', () => {
+    component.loginForm.controls['email'].patchValue('');
+    component.loginForm.controls['password'].patchValue('');
+    expect(component.loginForm.valid).toBeFalsy();
+  });
+
+  it('should be call onSubmit', () => {
+    component.onSubmit({});
+
+    expect(spy).toHaveBeenCalled();
+    expect(spy.calls.any()).toBeTruthy();
+  });
+
+  it('should be create form', () => {
+    component.ngOnInit();
+    expect(component.loginForm).not.toBeUndefined();
+  });
+
+  it('email should be type AbstractControl', () => {
+    const email = component.email;
+    expect(email instanceof AbstractControl).toBeTruthy();
+  });
+
+  it('pass should be type AbstractControl', () => {
+    const pass = component.pass;
+    expect(pass instanceof AbstractControl).toBeTruthy();
   });
 });
--- client/src/app/public/components/registration/registration.component.spec.ts
+++ client/src/app/public/components/registration/registration.component.spec.ts
@@ -1,14 +1,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormsModule,
+  ReactiveFormsModule,
+  AbstractControl,
+} from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientModule } from '@angular/common/http';
+import { DebugElement } from '@angular/core';
 
+import { LoginService } from './../../../core/services/auth/login.service';
 import { RegistrationComponent } from './registration.component';
 import { HeaderComponent } from '../header/header.component';
+import { of } from 'rxjs';
 
 describe('RegistrationComponent', () => {
   let component: RegistrationComponent;
   let fixture: ComponentFixture<RegistrationComponent>;
+  let de: DebugElement;
+  let spy: jasmine.Spy;
+  let service: LoginService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -25,10 +35,48 @@ describe('RegistrationComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(RegistrationComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement;
     fixture.detectChanges();
+
+    service = de.injector.get(LoginService);
+    spy = spyOn(service, 'registration').and.returnValue(of(null));
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('form should be valid', () => {
+    component.regForm.controls['email'].patchValue('some@gmail.com');
+    component.regForm.controls['password'].patchValue('password');
+    expect(component.regForm.valid).toBeTruthy();
+  });
+
+  it('form should be invalid', () => {
+    component.regForm.controls['email'].patchValue('');
+    component.regForm.controls['password'].patchValue('');
+    expect(component.regForm.valid).toBeFalsy();
+  });
+
+  it('should be call onSubmit', () => {
+    component.onSubmit({});
+
+    expect(spy).toHaveBeenCalled();
+    expect(spy.calls.any()).toBeTruthy();
+  });
+
+  it('should be create form', () => {
+    component.ngOnInit();
+    expect(component.regForm).not.toBeUndefined();
+  });
+
+  it('email should be type AbstractControl', () => {
+    const email = component.email;
+    expect(email instanceof AbstractControl).toBeTruthy();
+  });
+
+  it('pass should be type AbstractControl', () => {
+    const pass = component.pass;
+    expect(pass instanceof AbstractControl).toBeTruthy();
   });
 });
