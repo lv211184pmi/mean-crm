854d7f3a26c67493edf3781b4b3838d1920267ea
Maryniuk-SV <mefissto.w@gmail.com>
2019-04-23 17:47:11 +0300

    app component tested

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,9 +1,18 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { Router } from '@angular/router';
+import { DebugElement } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AuthService } from './core/services/auth/auth.service';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let de: DebugElement;
+  let spy: jasmine.Spy;
+  let service: AuthService;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AppComponent],
@@ -19,9 +28,21 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
+  beforeEach(async(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    de = fixture.debugElement;
+    service = de.injector.get(AuthService);
+    spy = spyOn(service, 'setToken');
+  }));
+
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
+  }));
+
+  it('should call AuthService', async(() => {
+    localStorage.setItem('token', 'mock token');
+    component.ngOnInit();
+    expect(spy.calls.any()).toBeTruthy();
   }));
 });
